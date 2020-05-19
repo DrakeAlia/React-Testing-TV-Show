ff230b1e3a611b98efa51e1c62f5260093deea32
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-05-18 19:31:11 -0700

    added data testing in episodes

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -3,14 +3,16 @@ import parse from 'html-react-parser';
 
 export default function Episodes(props) {
   return (
-    <div className="episodes">
+    // testing episode ids 
+    <div data-testid="episodes-id" className="episodes">
       {props.episodes.map(e => (
         <div className="episode" key={e.id}>
           {e.image && (
             <img className="episode-image" src={e.image.medium} alt={e.name} />
           )}
           <div className="episode-info">
-            <p className="episode-number">
+            {/* tests episode id numbers  */}
+          <p data-testid="episode-number-id" className="episode-number">
               Season {e.season}, Episode {e.number}
             </p>
             <h3>{e.name}</h3>
